d4cfcf0f8adb148a458a7f617e17b607327b0bb2
happyplus <894860970@qq.com>
2024-04-08 19:32:01 +0800

    feat: demo

--- autox/bridge.ts
+++ autox/bridge.ts
@@ -3,8 +3,9 @@ module.exports = [
         name: 'getDeviceBattery',
         callback: (data: string, cb: (data: string) => void) => {
             console.log("received web count :" + data);
-            toast(data);
-            cb(device.getBattery().toFixed(0));
+            toast('received web count :' + data);
+            let battery = device.getBattery().toFixed(0);
+            cb(battery);
         }
     }
 ]
