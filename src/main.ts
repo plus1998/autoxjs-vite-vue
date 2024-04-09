59ebb457d152b0251334f8255d26542ce9591361
happyplus <894860970@qq.com>
2024-04-09 15:36:42 +0800

    fix: css

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-
-if (process.env.NODE_ENV === 'development') {
-    import('vant/lib/index.css');
-}
+import 'vant/lib/index.css'
 
 createApp(App).use(router).mount('#app')
